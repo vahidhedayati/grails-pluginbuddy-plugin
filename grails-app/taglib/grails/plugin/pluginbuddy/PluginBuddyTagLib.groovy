1874bc18575eb31327c1b92ce0626bdfc680a08e
Vahid <vahid.hedayati@gmail.com>
2014-08-25 16:09:35 +0100

    .

--- grails-app/taglib/grails/plugin/pluginbuddy/PluginBuddyTagLib.groovy
+++ grails-app/taglib/grails/plugin/pluginbuddy/PluginBuddyTagLib.groovy
@@ -2,7 +2,7 @@ package grails.plugin.pluginbuddy
 
 class PluginBuddyTagLib {
 	static namespace = "enduser"
-	
+	static returnObjectForTags = ['verifyAppVersion']
 	def pluginbuddyService
 	
 	def verifyAppVersion={attrs, body ->
